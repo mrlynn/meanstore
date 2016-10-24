68df945c0c2484e1cb7c9d0bc10c94d2c5b29e85
Michael Lynn <merlynn@gmail.com>
2016-10-24 10:15:51 -0400

    completing signin module

--- config/passport.js
+++ config/passport.js
@@ -45,3 +45,33 @@ passport.use('local.signup', new LocalStrategy({
 			});
 		});
 }));
+
+passport.use('local.signin', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true
+}, function(req, email, password, done) {
+	req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+	req.checkBody('password', 'Invalid Password').notEmpty();
+	var errors = req.validationErrors();
+	if (errors) {
+		var messages = [];
+		errors.forEach(function(error) {
+			messages.push(error.msg);
+		});
+		return done(null, false, req.flash('error', messages));
+	}
+	User.findOne({'email': email}, function(err, user) {
+		if (err) {
+			return done(err);
+		}
+		if (!user) {
+			return done(null, false, {message: 'Invalid credentials.'});
+		}
+		if (!user.validPassword(password)) {
+			return done(null, false, {message: 'Invalid credentials.'});
+		}
+		return done(null, user);
+	});
+}));
+
