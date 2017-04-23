7155a569b4691e33d504a73884b3cbf4b650a7e1
Michael Lynn <merlynn@gmail.com>
2017-04-23 17:49:15 -0400

    remove project specific input fields

--- provision-vagrant.sh
+++ provision-vagrant.sh
@@ -138,7 +138,6 @@ function generate_data() {
 	node data/fake-cameras.js
 	node data/fake-printers.js
 	node data/fake-apparel.js
-	node data/fake-users.js
 	node data/fake-events.js
 	node data/fake-stores.js
 	node data/category-seeder.js
